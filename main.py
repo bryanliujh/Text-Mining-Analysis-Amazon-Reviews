0576b7540efe949dad3112beae864e62bf4db8d5
bryanliujh <bbx54ever@hotmail.com>
2018-09-29 16:07:12 +0800

    do cleaning first then noun phrase extract

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import nltk
 from nltk.corpus import stopwords
-import json
+import json, os, random, string
 import en_core_web_sm
 from tqdm import tqdm
 import multiprocessing
 from collections import Counter
-import os
+from nltk.stem import PorterStemmer
 
 noun_phrase_dict = {}
+nlp = en_core_web_sm.load()
+ps = PorterStemmer()
+stops = set(stopwords.words("english") + list(string.punctuation))
 
 def load_json_file():
     data = []
     noun_phrase_no_stop = {}
-    stops = set(stopwords.words("english"))
-    nlp = en_core_web_sm.load()
     filename_noun_phrase = 'noun_phrase_text.txt'
+
+
     #nltk.download('punkt')
     #python -m nltk.downloader stopwords
     #to use cell phone review for deployment, use sample review for testing
-    filename = 'CellPhoneReview.json'
-    #filename = 'SampleReview.json'
+    #filename = 'CellPhoneReview.json'
+    filename = 'SampleReview.json'
     with open(filename) as file:
         for line in file:
             data.append(json.loads(line))
-
-
-    find_top_3_popular_product(data)
 
     exists = os.path.isfile(filename_noun_phrase)
     if not exists:
@@ -33,7 +33,9 @@ def load_json_file():
         #parsing of large corpus could take up to 2 hours!!
         pool = multiprocessing.Pool(processes=multiprocessing.cpu_count()-1)
         for data_item in tqdm(data):
-            pool.apply_async(extract_noun_phrases(data_item['reviewText'].lower(), noun_phrase_dict, nlp), args=[data_item])
+            sentence_arr = clean_sentence(data_item['reviewText'])
+            for sentence in sentence_arr:
+                pool.apply_async(extract_noun_phrases(sentence, noun_phrase_dict, nlp), args=[data_item])
         pool.close()
         pool.join()
 
@@ -48,7 +50,8 @@ def load_json_file():
             json.dump(noun_phrase_no_stop, file)
             file.close()
 
-    print('hello')
+    find_top_3_popular_product(data)
+    find_top_20_noun_phrase(filename_noun_phrase)
 
 
 def extract_noun_phrases(sentence, noun_phrase_dict, nlp):
@@ -63,14 +66,50 @@ def extract_noun_phrases(sentence, noun_phrase_dict, nlp):
                 noun_phrase_dict[noun_phrase.text] = 1
 
 
+def clean_sentence(sentences):
+    #set to lowercase, break sentences into sentence, break sentence into words, remove stopwords and stem words
+    sentences = nltk.sent_tokenize(sentences.lower())
+    sentence_arr = []
+
+    for sentence in sentences:
+        words = nltk.word_tokenize(sentence)
+        w_list = []
+        s = ''
+        for w in words:
+            if w not in stops:
+                #w = ps.stem(w)
+                w_list.append(w)
+        s = ' '.join(w_list)
+        sentence_arr.append(s)
+
+    return sentence_arr
+
+
+
 def find_top_3_popular_product(data):
     count = Counter(data_item['asin'] for data_item in data)
     print(count)
 
 
-def find_top_20_noun_phrase():
-    print("hi")
+def find_top_20_noun_phrase(filename_noun_phrase):
+    with open(filename_noun_phrase, 'r') as file:
+        data = json.load(file)
+        first20pairs = {k: data[k] for k in list(data)[:20]}
+        print("Top 20 noun phrase: " + json.dumps(first20pairs))
 
+        file.close()
+
+
+def random_sample_5reviews(filename_noun_phrase):
+    random_review_dict = {}
+    with open(filename_noun_phrase, 'r') as file:
+        data = json.load(file)
+        for i in range(0,5):
+            rand_key = random.choice(data)
+            nlp(data[rand_key]).noun_chunks
+        print("Top 20 noun p: " + json.dumps())
+
+        file.close()
 
 
 if __name__ == '__main__':
